4f2bc391da9c78353379fc1c2b87bb7da33ab277
Yu-Hsuan Guan <yhguan8128@gmail.com>
2016-08-28 20:46:04 +0800

    * C-style string

--- main.cpp
+++ main.cpp
@@ -9,6 +9,44 @@ class String
     char * m_buf;
 
     public:
+        String(void):
+            m_size(0),
+            m_buf(NULL)
+        {
+
+        }
+
+        String(const char * buf):
+            m_size(0),
+            m_buf(NULL)
+        {
+            int s = strlen(buf);
+            m_buf = new char[s+1];
+            strcpy(m_buf, buf);
+            m_size = s;
+
+        }
+
+        bool empty(void) const
+        {
+            return m_size == 0;
+        }
+
+        void show(void) const
+        {
+            if (empty())
+            {
+                printf("string is empty!\n");
+                return;
+            }
+
+            printf("%s\n", m_buf);
+        }
+
+        const char * c_str(void) const
+        {
+            return m_buf;
+        }
 };
 
 
@@ -16,6 +54,8 @@ class String
 int main(int argc, const char * argv[])
 {
 
+    String str("hello");
+    printf("%s\n", str.c_str());
 
     return 0;
 }
